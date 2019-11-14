7757d99eae39de5453bb91fbf14d7bafe1bbb813
Ayan-Home <advin4603@gmail.com>
2019-11-14 12:48:12 +0530

    Added basic Dunder add method

--- Implementaton1.py
+++ Implementaton1.py
@@ -3,5 +3,5 @@ from Vectors2d import *
 f1 = Vector2d(x=5.6, y=6.2)
 f2 = Vector2d(x=1, y=1)
 
-print(f1.sum(x=7).math_form(), '+', f2.math_form())
-print('='+f1.resultant(f2).math_form())
+f3 = f1 + f2
+print(f3.val)
--- Vectors2d.py
+++ Vectors2d.py
@@ -1,5 +1,5 @@
 class Vector2d:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0.0 + 0j, y=0.0 + 0j):
         self.val = {'X': x, 'Y': y}
         Vector2d.system = '2 Dimensional'
 
@@ -13,7 +13,7 @@ class Vector2d:
         self.val['X'], self.val['Y'] = self.val['X'] + x, self.val['Y'] + y
         self.val = {'X': self.val['X'], 'Y': self.val['Y']}
 
-    def sum(self,x=0,y=0):
+    def sum(self, x=0, y=0):
         # Same as add_value but returns the sum as a new instance
         return Vector2d(x=self.val['X'] + x, y=self.val['Y'] + y)
 
@@ -24,10 +24,25 @@ class Vector2d:
         self.val['X'] += obj.val['Y']
         self.val['Y'] += obj.val['Y']
 
-    def resultant(self, obj):
+    def __add__(self, obj):
         # Requires 1 Positional Argument of Vector2d class
         # Returns Resultant of instance and the given object
-        return Vector2d(x=self.val['X'] + obj.val['X'], y=self.val['Y'] + obj.val['Y'])
+        if isinstance(obj, Vector2d):
+            return Vector2d(x=self.val['X'] + obj.val['X'], y=self.val['Y'] + obj.val['Y'])
+        elif isinstance(obj, tuple) or isinstance(obj, list):
+            if len(obj) == 2:
+                return Vector2d(x=self.val['X'] + obj[0], y=self.val['Y'] + obj[1])
+        elif isinstance(obj, str):
+            if ((obj.split()[0][-1] in 'iIxXîÎ') and (obj.split()[1][-1] in 'jJyYĵĴ')) or (
+                    (obj.split()[1][-1] in 'iIxXîÎ') and (obj.split()[0][-1] in 'jJyYĵĴ')):
+                try:
+                    return Vector2d(x=self.val['X'] + float(obj.split()[0][:-1]),
+                                    y=self.val['Y'] + float(obj.split()[1][:-1]))
+                except TypeError:
+                    return Vector2d(x=self.val['X'] + complex(obj.split()[0][:-1]),
+                                    y=self.val['Y'] + complex(obj.split()[1][:-1]))
+            elif obj.split()[0].isdigit() and obj.split()[1].isdigit():
+                return Vector2d(x=self.val['X'] + float(obj.split()[0]), y=self.val['Y'] + float(obj.split()[1]))
 
     def multiply_scalar(self, scalar):
         # Requires 1 Positional Argument of type number
